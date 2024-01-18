cffbe93d775f6abb5b640e10e1750ff92a9459f1
Daniel Echeverri <echeverri121@gmail.com>
2024-01-18 21:49:12 +0000

    Finish AddTopping method

--- ContosoPizza/Services/PizzaService.cs
+++ ContosoPizza/Services/PizzaService.cs
@@ -36,10 +36,25 @@ public class PizzaService
     return newPizza;
 }
 
-    public void AddTopping(int PizzaId, int ToppingId)
+    public void AddTopping(int pizzaId, int toppingId)
+{
+    var pizzaToUpdate = _context.Pizzas.Find(pizzaId);
+    var toppingToAdd = _context.Toppings.Find(toppingId);
+
+    if (pizzaToUpdate is null || toppingToAdd is null)
     {
-        throw new NotImplementedException();
+        throw new InvalidOperationException("Pizza or topping does not exist");
     }
+
+    if(pizzaToUpdate.Toppings is null)
+    {
+        pizzaToUpdate.Toppings = new List<Topping>();
+    }
+
+    pizzaToUpdate.Toppings.Add(toppingToAdd);
+
+    _context.SaveChanges();
+}
 
     public void UpdateSauce(int PizzaId, int SauceId)
     {

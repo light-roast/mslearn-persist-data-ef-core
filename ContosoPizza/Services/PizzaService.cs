50fb2ccc08369c5bd31d44d7ea613085c646c38a
Daniel Echeverri <echeverri121@gmail.com>
2024-01-18 21:36:26 +0000

    First two read operations

--- ContosoPizza/Services/PizzaService.cs
+++ ContosoPizza/Services/PizzaService.cs
@@ -14,13 +14,19 @@ public class PizzaService
 
     public IEnumerable<Pizza> GetAll()
     {
-        throw new NotImplementedException();
+        return _context.Pizzas
+            .AsNoTracking()
+            .ToList();
     }
 
     public Pizza? GetById(int id)
-    {
-        throw new NotImplementedException();
-    }
+{
+    return _context.Pizzas
+        .Include(p => p.Toppings)
+        .Include(p => p.Sauce)
+        .AsNoTracking()
+        .SingleOrDefault(p => p.Id == id);
+}
 
     public Pizza? Create(Pizza newPizza)
     {
